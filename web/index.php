1d3978f1a755177680cefe0094ca0095428be084
Gabriele Barcella <barcigabri@gmail.com>
2017-11-06 18:37:31 +0100

    Demo

--- web/index.php
+++ web/index.php
@@ -17,9 +17,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Our web handlers
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
 });
 
 $app->run();
