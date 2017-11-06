f535ad8dc4bbfe47f0645390ffd869a9e3785f55
Gabriele Barcella <barcigabri@gmail.com>
2017-11-06 18:39:09 +0100

    Demo

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return str_repeat('Hello', getenv('TIMES'));
 });
 
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
 $app->run();
