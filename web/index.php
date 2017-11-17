35fff4275c202bd6b0bff8fc6f5ad498bab6c777
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-11-17 10:12:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,84 +1,16 @@
 <?php
-
-$counter_logfile = 'visitor_counter.txt';  // will used to store count! This file should contain beside this script file. 
-// 5 minit in seconds
-$inactive = 10;  // how many second? Set the session max lifetime ? 1m = 60s 
-ini_set('session.gc_maxlifetime', $inactive); // set the session max lifetime 
-
-session_start();
-
-if (isset($_SESSION['testing']) && (time() - $_SESSION['testing'] > $inactive)) {
-    // last request was more than 2 hours ago
-    session_unset();     // unset $_SESSION variable for this page
-    session_destroy();   // destroy session data
-}
-
-$_SESSION['testing'] = time(); // Update session
-if( isset( $_SESSION['visit_count'] ) )
-   {
-      $_SESSION['visit_count'] += 1;
-   }
-   else
-   {
-      $_SESSION['visit_count'] = 1;
-   } 
-
-   if($_SESSION['visit_count']<2) { 
-
-      // Open log file for reading and writing
-      if ($vfile = @fopen($counter_logfile, 'r+'))
-      {
-      // Lock log file from other scripts
-         $locked = flock($vfile, LOCK_EX);
-
-         // Lock successful?
-         if ($locked)
-         {
-         // Let's read current count
-            $count = intval( trim( fread($vfile, filesize($counter_logfile) ) ) );
-
-            // Update count by 1 and write the new value to the log file
-            $count = $count + 1;
-            rewind($vfile);
-            fwrite($vfile, $count);
-
-         }
-         else
-         {
-         // Lock not successful. Better to ignore than to damage the log file
-            $count = 1;
-         }
-
-         // Release file lock and close file handle
-         flock($vfile, LOCK_UN);
-         fclose($vfile);
-      } 
-   }
-
+    setcookie('contatore', isset($_COOKIE['contatore']) ? $_COOKIE['contatore']++ : 1, time() + (86400*7), "/");
+    $ContaVisite = $_COOKIE['contatore'];
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<style>
-	body {
-		text-align: center;
-		font-family: sans-serif;
-	}
-	.title {
-		padding-top: 50px;
-		padding-bottom: 50px;
-	}
-	.visitor-container {
-		padding-bottom: 40px;
-		font-size: 18px;
-
-	}
-</style>
-<head>
-	<meta charset="UTF-8">
-	<title>Simple PHP Session Visitor hit Counter Demo Page</title>
-</head>
-<body>
-	ciao
-</body>
+<html> 
+    <head> 
+        <title>Contatore accessi</title> 
+    </head> 
+    <body> 
+        <?if ($ContaVisite == 1): ?>
+            Benvenuto! E' la prima volta che visiti questa pagina!. 
+        <?else:?> 
+            Hai già visitato questa pagina <?= $_COOKIE['contatore'] ?> volte. 
+        <?endif;?>
+    </body> 
 </html>
