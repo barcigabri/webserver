c0536f4276239547f2656f1b406449242999899b
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-11-17 09:46:20 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,62 +1,90 @@
 <?php
 
-require('../vendor/autoload.php');
+$counter_logfile = 'visitor_counter.txt';  // will used to store count! This file should contain beside this script file. 
+// 5 minit in seconds
+$inactive = 300;  // how many second? Set the session max lifetime ? 1m = 60s 
+ini_set('session.gc_maxlifetime', $inactive); // set the session max lifetime 
 
-$app = new Silex\Application();
-$app['debug'] = true;
+session_start();
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+if (isset($_SESSION['testing']) && (time() - $_SESSION['testing'] > $inactive)) {
+    // last request was more than 2 hours ago
+    session_unset();     // unset $_SESSION variable for this page
+    session_destroy();   // destroy session data
+}
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
-// Our web handlers
+$_SESSION['testing'] = time(); // Update session
+if( isset( $_SESSION['visit_count'] ) )
+   {
+      $_SESSION['visit_count'] += 1;
+   }
+   else
+   {
+      $_SESSION['visit_count'] = 1;
+   } 
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'CIAO CLASSE!';
-});
+   if($_SESSION['visit_count']<2) { 
 
-$app->get('/cowsay', function() use($app) {
-  $app['monolog']->addDebug('cowsay');
-  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
+      // Open log file for reading and writing
+      if ($vfile = @fopen($counter_logfile, 'r+'))
+      {
+      // Lock log file from other scripts
+         $locked = flock($vfile, LOCK_EX);
 
-$app->get('/polenta', function() use($app) {
-  $app['monolog']->addDebug('Polenta');
-  return 'La polenta a colaziù!';
-});
+         // Lock successful?
+         if ($locked)
+         {
+         // Let's read current count
+            $count = intval( trim( fread($vfile, filesize($counter_logfile) ) ) );
 
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
+            // Update count by 1 and write the new value to the log file
+            $count = $count + 1;
+            rewind($vfile);
+            fwrite($vfile, $count);
 
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
+         }
+         else
+         {
+         // Lock not successful. Better to ignore than to damage the log file
+            $count = 1;
+         }
 
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
+         // Release file lock and close file handle
+         flock($vfile, LOCK_UN);
+         fclose($vfile);
+      } 
+   }
 
-$app->run();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+	body {
+		text-align: center;
+		font-family: sans-serif;
+	}
+	.title {
+		padding-top: 50px;
+		padding-bottom: 50px;
+	}
+	.visitor-container {
+		padding-bottom: 40px;
+		font-size: 18px;
+
+	}
+</style>
+<head>
+	<meta charset="UTF-8">
+	<title>Simple PHP Session Visitor hit Counter Demo Page</title>
+</head>
+<body>
+	<h3 class="title"> Simple PHP Session Visitor hit Counter Demo Page </h3>
+	<p class="visitor-container"> Page has been Visited <?php include('visitor_counter.txt'); ?> Times </p> 
+
+	<p>If you visit this page after 5 minuet (adjustable) or from different web browser,<br> you will be counted  as new visitor</p>
+	
+	<p>Download this simple php visitor hit counter from gitHun <br>@ 
+	<a href="https://github.com/shariarbd/Simple-PHP-Session-Visitor-hit-Counter">Simple PHP Session Visitor hit Counter</a></p>
+</body>
+</html>
