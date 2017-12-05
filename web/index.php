7338a10ccd74221c66015b6432c958bbe183fd31
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 21:34:10 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,22 @@
 <?php
-echo "mao!";
+
+# Questo script chiama una pagina dal web come se fosse un browser, 
+# e poi la immagazzina in una variabile
+
+# Indirizzo della pagina da richiamare
+$indirizzo_pagina="www.giacobbe85.altervista.org";
+
+# Codice di utilizzo di cURL. 
+# Chiama la pagina e la immagazzina in $data
+$ch = curl_init() or die(curl_error());
+curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data=curl_exec($ch) or die(curl_error());
+
+# Stampa della variabile $data. 
+echo $data;
+
+# Stampa di eventuali errori
+echo curl_error($ch);
+curl_close($ch);
 ?>
