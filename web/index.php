8d8907d6bb6c62c76d3cd4a492575ce4a7d4cd72
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 16:28:43 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,24 +1,10 @@
-<?php
-
-$name = "visitCount";
-
-if (!isset($_COOKIE[$name])) {
-    $_COOKIE[$name] = 0;
-}
-$_COOKIE[$name] = 1 + (int) max(0, $_COOKIE[$name]);
-$result = setcookie($name, $_COOKIE[$name]);
-if (!$result) {
-    throw new RuntimeException("Failed to set cookie \"$name\"");
-}
 <html> 
     <head> 
-        <title>Count Page Access</title> 
+        <title>Pagina di Prova</title> 
     </head> 
     <body> 
-        <?if ($_COOKIE['$name']== 1): ?>
-            Welcome! This is the first time you have viewed this page. 
-        <?else:?> 
-            You have viewed this page <?= $_COOKIE['$name'] ?> times. 
-        <?endif;?>
+        <?
+            echo "Web App!!";
+        ?>
     </body> 
 </html>
