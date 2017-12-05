6d870709f38799c938d649cf6922c2268fbc6799
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 22:33:47 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,23 +1,44 @@
 <?php
 
-# Questo script chiama una pagina dal web come se fosse un browser, 
-# e poi la immagazzina in una variabile
+	# Questo script chiama un'API e la inserisce in una tabella 
 
-# Indirizzo della pagina da richiamare
-$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=pizzeria&limit=3&intent=checkin&client_id=YVMN1NGHAW4DWINOY2BHBVQTGR0RG01D4EVZ3Z3TPRN5EBE2&client_secret=GYRAVQCTVV5DUYI3J3OH2GKLQN5S2LEA0QIGECJ1MUFBTX2X&near=Bergamo%2CIT";
 
-# Codice di utilizzo di cURL. 
-# Chiama la pagina e la immagazzina in $data
-$ch = curl_init() or die(curl_error());
-curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$json=curl_exec($ch) or die(curl_error());
-#decodifico
-$data = json_decode($json);
-# Stampa della variabile $data. 
-echo $data->response->venues[0]->name;
+	# Indirizzo dell'API da richiedere
+	$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=pizzeria&limit=3&intent=checkin&client_id=YVMN1NGHAW4DWINOY2BHBVQTGR0RG01D4EVZ3Z3TPRN5EBE2&client_secret=GYRAVQCTVV5DUYI3J3OH2GKLQN5S2LEA0QIGECJ1MUFBTX2X&near=Bergamo%2CIT";
 
-# Stampa di eventuali errori
-echo curl_error($ch);
-curl_close($ch);
+	# Codice di utilizzo di cURL
+	# Chiama l'API e la immagazzina in $json
+	$ch = curl_init() or die(curl_error());
+	curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$json=curl_exec($ch) or die(curl_error());
+
+	# Decodifico la stringa json e la salvo nella variabile $data
+	$data = json_decode($json);
+
+	# Stampa della tabella delle pizzerie.
+	echo "<table>";
+		echo "<tr>";
+			echo "<th>NOME</th>";
+			echo "<th>LATITUDINE</th>";
+			echo "<th>LONGITUDINE</th>";
+		echo "</tr>";
+		for($i=0; $i<3; $i++)
+		{	
+			echo "<tr>";
+				echo "<td>";
+				echo $data->response->venues[$i]->name;
+				echo "</td>";
+				echo "<td>";
+				echo $data->response->venues[$i]->name;
+				echo "</td>";
+				echo "<td>";
+				echo $data->response->venues[$i]->name;
+				echo "</td>";
+			echo "</tr>";
+		}
+	echo "</table>";
+	# Stampa di eventuali errori
+	echo curl_error($ch);
+	curl_close($ch);
 ?>
