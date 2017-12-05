162f5fb19b01b23b8fede373d7c64e8a5ab5c659
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 16:42:43 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
-<html> 
-    <head> 
-        <title>Pagina di Prova</title> 
-    </head> 
-    <body> 
-        <?
-            echo "Web App!!";
-        ?>
-    </body> 
-</html>
+<?php
+$r = new HttpRequest('http://example.com/feed.rss', HttpRequest::METH_GET);
+$r->setOptions(array('lastmodified' => filemtime('local.rss')));
+$r->addQueryData(array('category' => 3));
+try {
+    $r->send();
+    if ($r->getResponseCode() == 200) {
+        file_put_contents('local.rss', $r->getResponseBody());
+    }
+} catch (HttpException $ex) {
+    echo $ex;
+}
+?>
