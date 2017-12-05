77bb736016574a0fc2a1d133707f23825ca41041
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 16:44:06 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,3 @@
 <?php
-$r = new HttpRequest('http://example.com/feed.rss', HttpRequest::METH_GET);
-$r->setOptions(array('lastmodified' => filemtime('local.rss')));
-$r->addQueryData(array('category' => 3));
-try {
-    $r->send();
-    if ($r->getResponseCode() == 200) {
-        file_put_contents('local.rss', $r->getResponseBody());
-    }
-} catch (HttpException $ex) {
-    echo $ex;
-}
+echo "mao!";
 ?>
