aa23008dfd1b66a3948b083c1f6483f88b9f55c5
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-05 21:55:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,8 +11,9 @@ $indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=
 $ch = curl_init() or die(curl_error());
 curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$data=curl_exec($ch) or die(curl_error());
-
+$json=curl_exec($ch) or die(curl_error());
+#decodifico
+$data = json_decode($json);
 # Stampa della variabile $data. 
 echo $data;
 
