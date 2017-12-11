cdee9ac9d9c5f796c8ac04e6d03830fa5a5fd176
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-11 18:46:41 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -53,7 +53,8 @@
 			# Decodifico la stringa json e la salvo nella variabile $data
 			$data = json_decode($json);
 			# Stampa della tabella delle pizzerie.
-			echo "prova:".$data->response->venues;
+			echo "prova:";
+			echo count($data->response->venues);
 			echo "<table>";
 				echo "<tr>";
 					echo "<th>NOME</th>";
