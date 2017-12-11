e091a718050810fde1c863b628552b2be3f75416
barcigabri <33417277+barcigabri@users.noreply.github.com>
2017-12-11 18:53:34 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -41,41 +41,52 @@
 			{
 				$que="pizzeria";
 			}
+			
 			# Questo script chiama un'API e la inserisce in una tabella
 			# Indirizzo dell'API da richiedere
 			$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$que&limit=$lim&intent=checkin&client_id=YVMN1NGHAW4DWINOY2BHBVQTGR0RG01D4EVZ3Z3TPRN5EBE2&client_secret=GYRAVQCTVV5DUYI3J3OH2GKLQN5S2LEA0QIGECJ1MUFBTX2X&near=$cit";
+			
 			# Codice di utilizzo di cURL
 			# Chiama l'API e la immagazzina in $json
 			$ch = curl_init() or die(curl_error());
 			curl_setopt($ch, CURLOPT_URL,$indirizzo_pagina);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			$json=curl_exec($ch) or die(curl_error());
+			
 			# Decodifico la stringa json e la salvo nella variabile $data
 			$data = json_decode($json);
+			
+			# Controllo valori
+			if (count($data->response->venues)<$lim);
+				$lim=$data->response->venues;
+			
 			# Stampa della tabella delle pizzerie.
-			echo "prova:";
-			echo count($data->response->venues);
-			echo "<table>";
-				echo "<tr>";
-					echo "<th>NOME</th>";
-					echo "<th>LATITUDINE</th>";
-					echo "<th>LONGITUDINE</th>";
-				echo "</tr>";
-				for($i=0; $i<$lim; $i++)
-				{
+			if($lim!=0)
+			{
+				echo "<table>";
 					echo "<tr>";
-						echo "<td>";
-						echo $data->response->venues[$i]->name;
-						echo "</td>";
-						echo "<td>";
-						echo $data->response->venues[$i]->location->lat;
-						echo "</td>";
-						echo "<td>";
-						echo $data->response->venues[$i]->location->lng;
-						echo "</td>";
+						echo "<th>NOME</th>";
+						echo "<th>LATITUDINE</th>";
+						echo "<th>LONGITUDINE</th>";
 					echo "</tr>";
-				}
-			echo "</table>";
+					for($i=0; $i<$lim; $i++)
+					{
+						echo "<tr>";
+							echo "<td>";
+							echo $data->response->venues[$i]->name;
+							echo "</td>";
+							echo "<td>";
+							echo $data->response->venues[$i]->location->lat;
+							echo "</td>";
+							echo "<td>";
+							echo $data->response->venues[$i]->location->lng;
+							echo "</td>";
+						echo "</tr>";
+					}
+				echo "</table>";
+			}
+			else
+				echo "ERRORE, NON SONO STATE TROVATE CORRISPONDENZE CON I CAMPI SELEZIONATI!";
 			# Stampa di eventuali errori
 			echo curl_error($ch);
 			curl_close($ch);
